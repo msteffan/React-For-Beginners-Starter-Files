e67fed64921240af6b6025b56c3033bcae4d7a8a
Melissa Steffan <msteffan2012@gmail.com>
2017-04-02 19:53:50 -0400

    add initial state and methods to add fish to the App component state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -6,6 +6,30 @@ import Inventory from './Inventory'
 
 
 class App extends React.Component {
+  constructor(){
+    //  inits the react component that we're extending
+    super();
+    this.addFish = this.addFish.bind(this)
+    // getInitialState
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  // ADD FISH
+  addFish(fish){
+    // best practice: get a copy of your current state and then update the state
+    // update state
+    const fishes = {...this.state.fishes};
+    // add in new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    this.setState({ fishes })
+  }
+
+  // RENDER
   render(){
     return (
       <div className="catch-of-the-day">
@@ -13,7 +37,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
